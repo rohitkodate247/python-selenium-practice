80c61ae9eff3fcc01a69b550dc1656d764df5aa6
Rohit Kodate <53255939+rohitkodate247@users.noreply.github.com>
2025-11-04 19:39:36 -0700

    First Test Run

---
+++ verify_chrome_driver.py
@@ -0,0 +1,9 @@
+# verify_chrome_driver.py
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
+print("✅ Chrome launched successfully.")
+driver.quit()
